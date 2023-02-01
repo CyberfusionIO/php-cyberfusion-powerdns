3572d46c80d664aa75bf26ead0fe37ab448da65c
Dick van der Heiden <d.vanderheiden@vdhicts.nl>
2023-02-01 21:24:10 +0100

    Example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cyberfusion\PowerDNS\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
